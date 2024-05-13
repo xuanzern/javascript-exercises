d563ca745549dff0042840e6c918812737de10c1
xz <xuanzernl@gmail.com>
2024-05-14 00:32:53 +0800

    complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    i = string.length-1;
+    output = ""
+    while (i >= 0){
+        output += string[i]
+        i--;
+    }
 
+    return output
 };
 
 // Do not edit below this line
