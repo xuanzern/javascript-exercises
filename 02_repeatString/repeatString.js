08ac31c184fcb833de3c20531726ce34a54c517b
xz <xuanzernl@gmail.com>
2024-05-14 00:27:59 +0800

    complete repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let output = ""
+    for (let i = 0; i < times; i++){
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
