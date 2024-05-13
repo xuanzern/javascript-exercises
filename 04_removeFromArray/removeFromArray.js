2b7ecebe0462cf103fb959f5064607f52e48194d
xz <xuanzernl@gmail.com>
2024-05-14 00:40:46 +0800

    complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const outArray = []
+    array.forEach((item) => {
+        if (!args.includes(item)){
+            outArray.push(item)
+        }
+    })
+    return outArray;
 };
 
 // Do not edit below this line
